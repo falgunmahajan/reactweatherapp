56463ae5348c257dff0ee77f4f9eeeeab16819c7
falgunmahajan <falgunmahajan1999@gmail.com>
2023-07-10 17:42:31 +0530

    applied some changes

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
     })();
   },[city])
   return (
-    <div className="App container-fluid text-white vh-100" style={{backgroundColor:"darkblue"}}>
+    <div className="App container-fluid text-white vh-100" style={{backgroundColor:"#00008b99"}}>
       <div className="row">
         <div className="col-md-4 my-5 m-auto">
           <h1>Weather App</h1>
